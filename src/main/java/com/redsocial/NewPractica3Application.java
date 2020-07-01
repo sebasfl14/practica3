95bf0d8fc1ab8852d9dd7287746e2302e09c5944
sebasfl.14 <sebasfl.14@hotmail.com>
2020-07-01 14:35:20 -0500

    practica3

---
+++ src/main/java/com/redsocial/NewPractica3Application.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPractica3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewPractica3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/redsocial/NewPractica3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPractica3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
